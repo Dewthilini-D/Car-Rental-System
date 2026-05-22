7f7cb274fd258b20e24cece14ea9c69f8c62d174
ඔයාගේ නම <dewthilini6@gmail.com>
2026-05-22 06:42:31 +0530

    Add car attributes and constructor

--- src/car.java
+++ src/car.java
@@ -1,3 +1,16 @@
-public class car {
-    
+class Car {
+    private String carId;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+    private boolean isAvailable;
+
+    public Car(String carId, String brand, String model, double basePricePerDay) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailable = true;
+    }
 }
+    
