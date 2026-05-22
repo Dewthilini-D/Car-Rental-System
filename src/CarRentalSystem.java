49846eb81114661e57cf3d4a9851b61339801d6c
ඔයාගේ නම <dewthilini6@gmail.com>
2026-05-22 07:22:34 +0530

    Add lists for cars, customers, and rentals

--- src/CarRentalSystem.java
+++ src/CarRentalSystem.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRentalSystem {
-    
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
+
+    public CarRentalSystem() {
+        cars = new ArrayList<>();
+        customers = new ArrayList<>();
+        rentals = new ArrayList<>();
+    }
 }
